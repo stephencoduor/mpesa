9a2dcd6f13b903810442a7290f2d3f383685a35e
stephen <stephencoduor@gmail.com>
2022-10-21 19:29:51 +0300

    Refactored the Config constructor
    Refactored the Config constructor

--- src/Config.php
+++ src/Config.php
@@ -13,17 +13,8 @@ class Config
     protected string  $shortcode;
     protected string  $store;
     protected string $pass_key;
-    protected string $initiator_name;
-    protected string $initiator_pass;
-    protected string $security_credential;
     protected string $TransactionType;
-//    protected string $timeout_url;
-//    protected string $validation_url;
-//    protected string $callback_url;
-//    protected string $result_url;
-    protected string $live_endpoint;
     protected string $sandbox_endpoint;
-
     public bool $environment;
 
     protected function __construct(array $config) {
@@ -47,16 +38,17 @@ class Config
         }
 
         $this->shortcode      = (isset($config['shortcode'])) ? $config['shortcode'] :null;
-        $this->initiator_name      = (isset($config['initiator_name'])) ? $config['initiator_name'] :null;
-        $this->initiator_pass      = (isset($config['initiator_pass'])) ? $config['initiator_pass'] :null;
         $this->store      = (isset($config['store'])) ? $config['store'] :null;
         $this->TransactionType      = (isset($config['TransactionType'])) ? $config['TransactionType'] :null;
+        $this->initiator_name      = (isset($config['initiator_name'])) ? $config['initiator_name'] :null;
+        $this->initiator_pass      = (isset($config['initiator_pass'])) ? $config['initiator_pass'] :null;
 
 //        $this->callback_url      = (isset($config['callback_url'])) ? $config['callback_url'] :null;
 //        $this->confirmation_url      = (isset($config['confirmation_url'])) ? $config['confirmation_url'] :null;
 //        $this->timeout_url      = (isset($config['timeout_url'])) ? $config['timeout_url'] :null;
 //        $this->validation_url      = (isset($config['validation_url'])) ? $config['validation_url'] :null;
 //        $this->result_url      = (isset($config['result_url'])) ? $config['result_url'] :null;
+
         
         $this->environment      = (isset($config['environment'])) ? $config['$environment'] :true;
         $this->live_endpoint      = 'https://api.safaricom.co.ke/';
